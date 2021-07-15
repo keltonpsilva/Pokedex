08d766294699fef26d0b2b21df60d400b542bbc4
Kelton Silva <kelton.jesus23@gmail.com>
2021-07-15 18:53:00 +0100

    Add project files.

---
+++ src/Pokedex.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pokedex.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public ActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
