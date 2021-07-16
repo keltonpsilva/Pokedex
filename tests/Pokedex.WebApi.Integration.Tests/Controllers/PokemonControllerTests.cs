771639ef2a0f6d22a71658f5e191e7fa100c245a
Kelton Silva <kelton.jesus23@gmail.com>
2021-07-16 12:27:39 +0100

    Add Pokemon controller
    - Add WebApi Unit tests project
    - Add WebApi Integration tests project

---
+++ tests/Pokedex.WebApi.Integration.Tests/Controllers/PokemonControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using Pokedex.Domain.DomainModel;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Pokedex.WebApi.Integration.Tests.Controllers
+{
+    [TestFixture]
+    public class PokemonControllerTests : IntegrationTestBaseClass
+    {
+        [Test]
+        public async Task Get_ValidPokemonName_ShouldReturnOkResponseWithPokemonData()
+        {
+            // Arrange
+            var pokemonName = "ditto";
+
+            // Act
+            var response = await Client.GetAsync($"api/pokemon/{pokemonName}");
+            var responseContent = JsonConvert.DeserializeObject<Pokemon>(await response.Content.ReadAsStringAsync());
+
+            // Assert
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(responseContent, Is.Not.Null);
+            Assert.That(responseContent.Name, Is.EqualTo(pokemonName));
+        }
+    }
+}
