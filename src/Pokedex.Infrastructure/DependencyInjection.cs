72844b74b7b60e1a0e737b833686c30aecca6fde
Kelton Silva <kelton.jesus23@gmail.com>
2021-07-16 12:54:37 +0100

    Add application external integration client

---
+++ src/Pokedex.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RestSharp;
+
+namespace Pokedex.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddSingleton<IRestClient, RestClient>();
+
+            return services;
+        }
+    }
+}
